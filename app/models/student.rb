078df061c2bc291e9e9e1804688d20f5b9163982
Jennifer Miller <jmillerarcher@me.com>
2020-03-19 13:04:07 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  def self.search(name)
+    if name == ''
+      self.all
+    else
+      self.all.select do |s|
+        s.name.downcase.include?(name)
+      end
+    end
+  end
 end
